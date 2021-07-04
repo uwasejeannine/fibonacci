666e5d68f42589400073d2a4b655474b2aedb8da
uwasejeannine <jeannineuwasee@gmail.com>
2021-07-04 22:49:13 +0300

    fibonacci

---
+++ app/src/androidTest/java/com/example/fibonacciactivity/number_list.kt
@@ -0,0 +1,31 @@
+package com.example.fibonacciactivity
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
+
+
+class number_list(public var numberList:List<BigInteger>):RecyclerView.Adapter<number_list.NumberRecyclerView>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberRecyclerView {
+        var num=LayoutInflater.from(parent.context).inflate(R.layout.number_list,
+        parent,false)
+       return NumberRecyclerView(num)
+    }
+
+    override fun onBindViewHolder(holder: NumberRecyclerView, position: Int) {
+        holder.tvnumber_list.text=numberList.get(position).toString()
+
+    }
+
+    override fun getItemCount(): Int {
+       return numberList.size
+    }
+
+    class NumberRecyclerView(num:View):RecyclerView.ViewHolder(num){
+        var tvnumber_list=num.findViewById<TextView>(R.id.tvnumberList)
+    }
+
+}
---
+++ app/src/main/java/com/example/fibonacciactivity/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fibonacciactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var rvListNumbers = findViewById<RecyclerView>(R.id.rvListNumbers)
+        rvListNumbers.layoutManager = LinearLayoutManager(baseContext)
+//        var numberAdapter = number_list (fibonacci(100))
+        rvListNumbers.adapter = number_list
+
+
+    }
+
+//    private fun number_list(fibonacci: List<BigInteger>): RecyclerView.Adapter<RecyclerView.ViewHolder>? {
+//return number_list(fibonacci)
+//    }
+
+
+}
+fun fibonacci(numbers: Int):List<BigInteger>{
+    var nums= MutableList<BigInteger>(numbers,{BigInteger.ZERO })
+    var number2=BigInteger.ZERO
+    var number3=BigInteger.ONE
+    nums[0]=number2
+    nums[1]=number3
+    for(a in 1..numbers){
+        var sum=number2+number3
+        number2=number3
+        number3=sum
+        nums[a-1]=number2
+
+    }
+    return nums
+}
